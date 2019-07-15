11c2484ff8c6cacf3ba31a3142f34937a3c35cb2
dougliebe <liebe.doug@gmail.com>
2019-07-15 13:16:36 -0400

    add refusal percentages and pretty graphs up

--- DMIplots.R
+++ DMIplots.R
@@ -15,8 +15,10 @@ d2 %>%
 ######### Avg Refusals by Day
 d2 %>%
   filter(Day >= 0 , Day < 9) %>%
+  group_by(Animal.ID) %>%
+  mutate(max.ref = Refusals.Adjusted.For.Target/max(Refusals.Adjusted.For.Target)) %>%
   group_by(Day) %>%
-  summarise(ref = mean(Refusals.Adjusted.For.Target, na.rm = T),n = n()) %>%
+  summarise(ref = mean(max.ref, na.rm = T),n = n()) %>%
   ggplot() +
     geom_line(aes(Day, ref))
 
@@ -33,26 +35,49 @@ d2 %>%
 d2 %>%
   filter(Day >= 0 , Day < 9) %>%
   group_by(Animal.ID) %>%
-  mutate(max.target = Total.Target/max(Total.Target)) %>%
+  mutate(max.target = Total.Target/max(Total.Target),
+         max.ref = Refusals.Adjusted.For.Target/max(Refusals.Adjusted.For.Target)) %>%
   ungroup() %>%
   group_by(Day) %>%
   summarise(target.day = mean(max.target, na.rm = T),
-            eff.day = mean(Eff.max, na.rm = T), n = n()) %>%
+            eff.day = mean(Eff.max, na.rm = T),
+            ref.day = mean(max.ref, na.rm = T),n = n()) %>%
   ggplot() +
-  geom_line(aes(Day, eff.day), color = "blue")+
-  geom_line(aes(Day, target.day), color = "red")+
-  ylab("Percent") 
+  geom_line(aes(Day, eff.day), color = "blue", size = 1)+
+  geom_line(aes(Day, target.day), color = "red", size = 1)+
+  geom_line(aes(Day, ref.day), color = "green", size = 1)+
+  ylab("Percent")
 
 ######### Efficiency compared to Target Fed per Animal
 d2 %>%
   filter(Day >= 0 , Day < 9) %>%
   group_by(Animal.ID) %>%
-  mutate(max.target = Total.Target/max(Total.Target)) %>%
+  mutate(max.target = Total.Target/max(Total.Target),
+         max.ref = Refusals.Adjusted.For.Target/max(Refusals.Adjusted.For.Target)) %>%
   ungroup() %>%
   group_by(Animal.ID, Day) %>%
   summarise(target.day = mean(max.target, na.rm = T),
-            eff.day = mean(Eff.max, na.rm = T), n = n()) %>%
+            eff.day = mean(Eff.max, na.rm = T),
+            ref.day = mean(max.ref, na.rm = T),n = n()) %>%
   ggplot() +
-  geom_line(aes(Day, eff.day), color = "blue")+
-  geom_line(aes(Day, target.day), color = "red")+
+  geom_line(aes(Day, eff.day), color = "blue", size = 1)+
+  geom_line(aes(Day, target.day), color = "red", size = 1)+
+  geom_line(aes(Day, ref.day), color = "green", size = 1)+
   ylab("Percent") + facet_wrap(~Animal.ID)
+
+######### Efficiency compared to Target Fed per TD
+d2 %>%
+  filter(Day >= 0 , Day < 9) %>%
+  group_by(Animal.ID) %>%
+  mutate(max.target = Total.Target/max(Total.Target),
+         max.ref = Refusals.Adjusted.For.Target/max(Refusals.Adjusted.For.Target)) %>%
+  ungroup() %>%
+  group_by(Top.Dress, Day) %>%
+  summarise(target.day = mean(max.target, na.rm = T),
+            eff.day = mean(Eff.max, na.rm = T),
+            ref.day = mean(max.ref, na.rm = T),n = n()) %>%
+  ggplot() +
+  geom_line(aes(Day, eff.day), color = "blue", size = 1)+
+  geom_line(aes(Day, target.day), color = "red", size = 1)+
+  geom_line(aes(Day, ref.day), color = "green", size = 1)+
+  ylab("Percent") + facet_wrap(~Top.Dress)
